5c8c1eece198ca45b1f864d038c93246e519db3d
SamThompson57 <s_thomo@hotmail.co.uk>
2024-02-01 18:52:47 +0000

    Remade using express, have left vanilla code commented in

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const http = require('node:http');
 const fs = require('fs');
 const url = require('node:url');
+const express = require('express');
+const app = express()
+const path = require("path");
+const { error } = require('node:console');
 
 const hostname = 'localhost';
 const port = 8080;
-
+/*
 function docSelector(doc){
 
     switch(doc){
@@ -40,4 +44,28 @@ const server = http.createServer(function(req,res){
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
-    });
+    });*/
+    
+   
+
+    app.get("/", (req,res) => {
+        res.sendFile(path.join(__dirname, '/index.html'))
+    })
+
+    app.get("/about", (req,res) => {
+        res.sendFile(path.join(__dirname, '/about.html'))
+    })
+
+    app.get("/contact-me", (req,res) => {
+        res.sendFile(path.join(__dirname, '/contact-me.html'))
+    })
+
+    app.use((req, res, next) => {
+        res.status(404).sendFile(path.join(__dirname, '/404.html'))
+      })
+  
+      
+    app.listen(port, () => {
+        console.log(`Server running at http://${hostname}:${port}/`)
+    })
+
